6b01c3f4f70b90aa9d2d34c5d899c9422f5a4ec6
kuodrek <kuodrek@gmail.com>
2022-07-04 19:25:03 -0300

    added runge-kutta while

--- main.py
+++ main.py
@@ -1,93 +1,32 @@
 import numpy as np
-import math
+from functions import takeoff
+from data import get_data
 
-# Descobrir o que é gama, teta e alfa nesse contexto
+dados_planilha = get_data()
+de_takeoff = dados_planilha['de_takeoff']
 
-g = 9.81
-rho = 1.086
-m = 1
-x_tdp = 1
-x_tdn = 1
-W = m*g
-Iyy = 0.3
-dt = 0
-Sref = 1
-mi = 1
+X_0 = np.zeros(6)
+U_0 = [0]
+U = U_0
+h = 0.25
 
-def tracao(V, rho):
-    T = 1
-    return T
+x_acionamento = dados_planilha['x_acionamento']
 
-def takeoff(X, U, aircraft_data):
-    Vx = X[0]
-    Vz = X[1]
-    q = X[2]
+t = 0
+t_vetor = []
+t_max = 20
+while True:
+    t += h
+    t_vetor.append(t)
+
     x_pos = X[3]
-    z_pos = X[4]
-    teta = X[5]
+    if x_pos > x_acionamento:
+        U = [de_takeoff]
 
-    de = U[0]
+    K1, data_output = takeoff(X, U, dados_planilha)
+    K2, _ = takeoff(X, U, dados_planilha)
+    K3, _ = takeoff(X, U, dados_planilha)
+    K4, _ = takeoff(X, U, dados_planilha)
+    X = X+h/6*(K1+2*K2+2*K3+K4)
 
-    alfa = teta
-    V = math.sqrt(Vx ** 2 + Vz ** 2)
-
-    # Dados avião
-    CL_alfa = aircraft_data['CL_alfa']
-    CL_de = aircraft_data['CL_de']
-    CL_q = aircraft_data['CL_q']
-    CL_0 = aircraft_data['CL_0']
-    Cm_alfa = aircraft_data['Cm_alfa']
-    Cm_de = aircraft_data['Cm_de']
-    Cm_q = aircraft_data['Cm_q']
-    Cm_0 = aircraft_data['Cm_0']
-
-    # Coeficientes
-    CL = CL_alfa * alfa + CL_de * de + CL_q * q + CL_0
-    CD = 1
-    Cm_cg = Cm_alfa * alfa + Cm_de * de + Cm_q * q + Cm_0
-
-    # Força e momento do motor
-    T = tracao(V, rho) * g
-    M_motor = T * dt
-
-    # Forças e momentos aerodinâmicos
-    L = 0.5 * rho * V ** 2 * Sref * CL
-    D = 0.5 * rho * V ** 2 * Sref * CD
-    M_cg = 0.5 * rho * V ** 2 * Sref * Cm_cg
-
-
-    # Equações das reações dos trens de pouso / nariz
-    # R_n = R_tdp + R_tdn
-    R_n = W - T*np.sin(teta) - L
-    R_tdn = (R_n - 1 / x_tdp * (M_cg + M_motor)) * (1 + x_tdn/x_tdp) ** -1
-    if R_tdn < 0: R_tdn = 0
-
-    R_tdp = 1 /(x_tdp) * (M_cg + M_motor + R_tdn*x_tdn)
-    P_cg = -R_tdp*x_tdp + R_tdn*x_tdn
-
-    if R_n >= 0:
-        # Reação dos trens de pouso e nariz > 0 -> Avião correndo na pista
-        # Referencia: runway
-        ax = (T*np.cos(teta) - D + W - mi*R_n)/m
-        az = 0
-        if R_tdn > 0:
-            # Reação do trem de nariz > 0 -> Avião correndo na pista (não rotacionou)
-            ateta = 0
-            teta_dot = 0
-        else:
-            # Reação do trem de nariz = 0 -> Avião rotacionando
-            ateta = (M_cg + M_motor + P_cg) / Iyy
-            teta_dot = q
-        x_pos_dot = Vx
-        z_pos_dot = 0
-    else:
-        # Reações dos trens de pouso e nariz = 0 -> Avião voando
-        # Referencia: horizon
-        alfa = Vz / Vx # verificar se usa essa formula nesse caso
-        gama = teta - alfa
-        ax = (T*np.cos(teta) - D*np.cos(gama) - L*np.sin(gama))/m
-        az = (T*np.sin(teta) + L*np.cos(gama) - W - D*np.sin(gama))/m
-        ateta = (M_cg + M_motor)/Iyy
-        x_pos_dot = Vx
-        z_pos_dot = Vz
-        teta_dot = q
+    if t > t_max: break
