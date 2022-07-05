36af5b649a206edaa400a9166c2774b0119cfda3
kuodrek <kuodrek@gmail.com>
2022-07-04 23:15:51 -0300

    updated variables

--- edi/main.py
+++ edi/main.py
@@ -1,8 +1,8 @@
 import numpy as np
 from functions import takeoff
-from data import get_data
+from data import get_data, get_data_teste
 
-dados_planilha = get_data()
+dados_planilha = get_data_teste()
 de_takeoff = dados_planilha['de_takeoff']
 
 X_0 = np.zeros(6)
@@ -10,14 +10,14 @@ U_0 = [0]
 U = U_0
 h = 0.25
 
-x_acionamento = dados_planilha['x_acionamento']
+ac_eh = dados_planilha['ac_eh']
 
 t = 0
 t_vetor = [t]
 t_max = 20
 while True:
     x_pos = X[3]
-    if x_pos > x_acionamento:
+    if x_pos > ac_eh:
         U = [de_takeoff]
 
     K1, data_output = takeoff(X, U, dados_planilha)
