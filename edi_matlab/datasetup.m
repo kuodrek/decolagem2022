e7bcfaa627a5d97f554c9e654fcd8894c1a9b992
kuodrek <kuodrek@gmail.com>
2022-07-10 02:22:28 -0300

    added drag variables

--- edi_matlab/datasetup.m
+++ edi_matlab/datasetup.m
@@ -31,7 +31,7 @@ x_tdn = geral(17,1);
 mi = geral(18,1);
 Sd = geral(19,1);
 z_inicial = geral(20,1);
-data{6,1} = Ib;
+data{7,1} = Ib;
 data{2,1} = [rho g Sref bref cref nh yp zp lp xcg zcg zh x_tdp x_tdn mi Sd z_inicial];
 
 superficies = xlsread('dinamicadados.xlsx','Superficies');
@@ -45,11 +45,8 @@ damax = superficies(6,1)*pi/180;
 iw = superficies(7,1)*pi/180;
 depsilondalpha = superficies(8,1);
 epsilon0 = superficies(9,1)*pi/180;
-CDw_c1 = superficies(10,1)*(180/pi)^2;
-CDw_c2 = superficies(11,1)*(180/pi);
-CDw_c3 = superficies(12,1);
-xac = superficies(13,1);
-CLmax = superficies(14,1);
+xac = superficies(10,1);
+CLmax = superficies(11,1);
 % Dados EH
 Sh = superficies(1,3);
 CLah = superficies(2,3)*180/pi;
@@ -59,9 +56,6 @@ demin = superficies(5,3)*pi/180;
 demax = superficies(6,3)*pi/180;
 ih = superficies(7,3)*pi/180;
 Vh = superficies(8,3);
-CDh_c1 = superficies(10,3)*(180/pi)^2;
-CDh_c2 = superficies(11,3)*(180/pi);
-CDh_c3 = superficies(12,3);
 % Dados EV
 drmin = superficies(5,5)*pi/180;
 drmax = superficies(6,5)*pi/180;
@@ -69,10 +63,34 @@ drmax = superficies(6,5)*pi/180;
 dtmin = superficies(1,7);
 dtmax = superficies(2,7);
 nhelice = superficies(3,7);
-data{3,1} = [Sw CLaw CL0w Cmacw damin damax iw depsilondalpha epsilon0 CDw_c1 CDw_c2 CDw_c3 xac CLmax;...
-             Sh CLah CL0h Cmach demin demax ih Vh 0 CDh_c1 CDh_c2 CDh_c3 0 0;...
-             0 0 0 0 drmin drmax 0 0 0 0 0 0 0 0;...
-             0 0 0 0 dtmin dtmax 0 0 0 0 0 0 0 0];
+data{3,1} = [Sw CLaw CL0w Cmacw damin damax iw depsilondalpha epsilon0 xac CLmax;...
+             Sh CLah CL0h Cmach demin demax ih Vh 0 0 0;...
+             0 0 0 0 drmin drmax 0 0 0 0 0;...
+             0 0 0 0 dtmin dtmax 0 0 0 0 0];
+% Arrastos
+arrastos = xlsread('dinamicadados.xlsx','Arrastos');
+CDpw_c1 = arrastos(1,1);
+CDpw_c2 = arrastos(2,1);
+CDiw_c1 = arrastos(4,1)*(180/pi)^2;
+CDiw_c2 = arrastos(5,1)*(180/pi);
+CDiw_c3 = arrastos(6,1);
+CDph_c1 = arrastos(1,4);
+CDph_c2 = arrastos(2,4);
+CDih_c1 = arrastos(4,4)*(180/pi)^2;
+CDih_c2 = arrastos(5,4)*(180/pi);
+CDih_c3 = arrastos(6,4);
+CDpv_c1 = arrastos(1,7);
+CDpv_c2 = arrastos(2,7);
+CDpf_c1 = arrastos(1,10);
+CDpf_c2 = arrastos(2,10);
+data{4,1} = [
+            CDpw_c1 CDpw_c2 0; ...
+            CDiw_c1 CDiw_c2 CDiw_c3; ...
+            CDph_c1 CDph_c2 0; ...
+            CDih_c1 CDih_c2 CDih_c3; ...
+            CDpv_c1 CDpv_c2 0; ...
+            CDpf_c1 CDpf_c2 0
+            ];
 
 % Derivadas
 derivadas = xlsread('dinamicadados.xlsx','Derivadas');
@@ -95,7 +113,7 @@ Cmde = derivadas(16,1);
 CYdr = derivadas(17,1);
 Cldr = derivadas(18,1);
 Cndr = derivadas(19,1);
-data{4,1} = [CYb Clb Cnb; CYp Clp Cnp; CLq Cmq 0; CYr Clr Cnr; CYda Clda Cnda; CLde Cmde 0; CYdr Cldr Cndr];
+data{5,1} = [CYb Clb Cnb; CYp Clp Cnp; CLq Cmq 0; CYr Clr Cnr; CYda Clda Cnda; CLde Cmde 0; CYdr Cldr Cndr];
 
 tracao = xlsread('dinamicadados.xlsx','Tracao');
 motorInput = zeros(1,22);
@@ -106,5 +124,5 @@ for i=1:size(motorInput,2)
         motorInput(1,i) = tracao(nhelice,i+2);
     end
 end
-data{5,1} = motorInput;
+data{6,1} = motorInput;
 end
